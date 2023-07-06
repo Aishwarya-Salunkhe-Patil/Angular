5e89b513bd4799caef40b5d9e9ed4455b17a2f19
Aishwarya <patil.aishwarya.programmer@gmail.com>
2023-07-06 19:33:34 +0530

    Angular Dotnet Cards App

---
+++ API/Cards1/Cards.API1/Models/Card.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cards.API.Models
+{
+    public class Card
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public String CardholderName { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public string CardNumber { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+        public int ExpiryMonth { get; set; }
+
+        public int ExpiryYear { get; set; }
+
+        public int CVC { get; set; }
+    }
+}
